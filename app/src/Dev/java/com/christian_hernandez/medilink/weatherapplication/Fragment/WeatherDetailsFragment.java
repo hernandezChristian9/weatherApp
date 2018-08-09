dfc069766d336e27c054dfb05afa3a23cf9e6fb3
Christian Hernandez <christian_hernandez@medilink.com.ph>
2018-08-10 00:32:43 +0800

    fifth commit

---
+++ app/src/Dev/java/com/christian_hernandez/medilink/weatherapplication/Fragment/WeatherDetailsFragment.java
@@ -0,0 +1,178 @@
+package com.christian_hernandez.medilink.weatherapplication.Fragment;
+
+
+import android.annotation.SuppressLint;
+import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.christian_hernandez.medilink.weatherapplication.Api.ApiMethods;
+import com.christian_hernandez.medilink.weatherapplication.Api.GlobalVariables;
+import com.christian_hernandez.medilink.weatherapplication.Model.London;
+import com.christian_hernandez.medilink.weatherapplication.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by christian_hernandez on 8/9/2018.
+ */
+
+public class WeatherDetailsFragment extends Fragment{
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private London london_details;
+
+    TextView tv_current_temperature_field, tv_weather_field, tv_details_field;
+    FragmentManager manager;
+    ImageView iv_weather_icon;
+
+    private Bitmap london_icon = null;
+
+    public WeatherDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    public static WeatherDetailsFragment newInstance(String param1, String param2) {
+        WeatherDetailsFragment fragment = new WeatherDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            london_details = (London) bundle.getSerializable("MY_BUNDLE");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
+        manager = getFragmentManager();
+
+        tv_current_temperature_field = (TextView) view.findViewById(R.id.tv_current_temperature_field);
+        tv_weather_field = (TextView) view.findViewById(R.id.tv_weather_field);
+        tv_details_field = (TextView) view.findViewById(R.id.tv_details_field);
+
+        iv_weather_icon = (ImageView) view.findViewById(R.id.iv_weather_icon);
+
+        tv_current_temperature_field.setText(GlobalVariables.london_datalist.getTemp()+"°C");
+        tv_weather_field.setText(GlobalVariables.london_datalist.getMain());
+        tv_details_field.setText(GlobalVariables.london_datalist.getDescription());
+
+        iv_weather_icon.setImageBitmap(GlobalVariables.london_datalist.getIcons());
+
+
+        return view;
+    }
+
+    private class GetDetails extends AsyncTask<String, Void, String> {
+        private ProgressDialog progressDialog = new ProgressDialog(getContext());
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setTitle("Weather App Message");
+            progressDialog.setMessage("Gathering Information..");
+            progressDialog.setCancelable(false);
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(String... strings) {
+            InputStream is = null;
+            try {
+                //GET DETAILS FOR LONDON WEATHER
+                JSONObject london = new ApiMethods(getContext()).GetLondonWeather();
+
+                JSONObject data = new JSONObject(london.getString("Data").replaceAll("null","\"\""));
+
+                JSONArray weather = data.getJSONArray("weather");
+                JSONObject JSONWeather = weather.getJSONObject(0);
+                GlobalVariables.london_datalist =  new London();
+                GlobalVariables.london_datalist.setId(String.valueOf(JSONWeather.get("id")));
+                GlobalVariables.london_datalist.setMain(String.valueOf(JSONWeather.get("main")));
+                GlobalVariables.london_datalist.setDescription(String.valueOf(JSONWeather.get("description")));
+                GlobalVariables.london_datalist.setIcon(String.valueOf(JSONWeather.get("icon")));
+
+                JSONObject main = new JSONObject(data.getString("main"));
+                GlobalVariables.london_datalist.setTemp(String.valueOf(main.get("temp")));
+
+                JSONObject wind = new JSONObject(data.getString("wind"));
+                GlobalVariables.london_datalist.setSpeed(String.valueOf(wind.get("speed")));
+
+
+
+                String urlOfLondonicon = "http://openweathermap.org/img/w/"+GlobalVariables.london_datalist.getIcon()+".png";
+                try {
+                    is = new URL(urlOfLondonicon).openStream();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                london_icon = BitmapFactory.decodeStream(is);
+                GlobalVariables.london_datalist.setIcons(london_icon);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tv_current_temperature_field.setText(GlobalVariables.london_datalist.getTemp()+"°C");
+            tv_weather_field.setText(GlobalVariables.london_datalist.getMain());
+            tv_details_field.setText(GlobalVariables.london_datalist.getDescription());
+
+            iv_weather_icon.setImageBitmap(GlobalVariables.london_datalist.getIcons());
+
+            progressDialog.dismiss();
+        }
+
+
+
+    }
+}
---
+++ app/src/Dev/java/com/christian_hernandez/medilink/weatherapplication/Model/London.java
@@ -0,0 +1,84 @@
+package com.christian_hernandez.medilink.weatherapplication.Model;
+
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+/**
+ * Created by christian_hernandez on 8/9/2018.
+ */
+
+public class London implements Serializable {
+    private String id;
+    private String main;
+    private String description;
+    private String icon;
+    private String temp;
+    private String speed;
+    private String deg;
+    private Bitmap icons;
+
+    public Bitmap getIcons() {
+        return icons;
+    }
+
+    public void setIcons(Bitmap icons) {
+        this.icons = icons;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getDeg() {
+        return deg;
+    }
+
+    public void setDeg(String deg) {
+        this.deg = deg;
+    }
+}
